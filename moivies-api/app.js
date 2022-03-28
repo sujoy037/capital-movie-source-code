442ad655f6790e4489b27b66bb657efbc43c68de
Pritam <www.pritamdas1999@gmail.com>
2022-03-28 21:16:45 +0530

    final up

--- moivies-api/app.js
+++ moivies-api/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
+const path = require('path')
 const cors = require("cors")
+const {db_url} = require('./api/config.json')
 const cookieParser = require("cookie-parser");
 const userRoutes = require('./api/routes/User');
 const movieRouter = require("./api/routes/movies")
@@ -10,14 +12,15 @@ const app = express();
 
 
 
-mongoose.connect('mongodb+srv://sujoy037:6o2anGPrthmznUy0@cluster0.akpdv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(db_url)
+
 
 mongoose.connection.on('error', err => {
     console.log('connection fail');
 })
 
 mongoose.connection.on('connected', connected => {
-    console.log('connected');
+    console.log('db connected');
 })
 
 
@@ -27,9 +30,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use('/', userRoutes);
+app.use(express.static(path.join(__dirname, "build")));
 
+
+app.use('/', userRoutes);
 app.use('/movies', movieRouter);
+
+
+
+// Web app
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
 
 app.use("*", (req, res)=>{
     return res.status(400).json({message: "Invalid URL"})
